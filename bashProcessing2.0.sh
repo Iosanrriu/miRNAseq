7fabbca4b3edea046177b11631c0bbd79609d7ac
Iosanrriu <70613042+Iosanrriu@users.noreply.github.com>
2021-12-02 15:33:51 -0300

    Add files via upload

--- bashProcessing2.0.sh
+++ bashProcessing2.0.sh
@@ -3,7 +3,7 @@
 #>>>>>>>definir rutas abosultas a las carpetas donde se almancenaran los indices de bowtie y los fastq
 
 pathfastq=/mnt/c/Users/espin/Desktop/dataSets/GSE147714
-pathIndex=/mnt/c/Users/espin/Desktop/Biospace/index
+pathIndex=/mnt/c/Users/espin/Desktop/index
 settings='--cores=6 -a TGGAATTCTCGGGTGCCAAGG -m 17 --quality-cutoff 32'
 export settings
 export pathfastq
@@ -12,12 +12,11 @@ export pathIndex
 #>>>>>>>download and transform sra to fastq files <sratoolkit>
 
 for ((i=42;i<=53;i++)); do
-    SRA=SRR114482$i
-    docker run -e SRA -v $pathfastq:/root/ncbi/public/sra 66ca1b570a7a && echo "$SRA"' sra->fastq succesfully' || echo  "$SRA"' fail to donwload'
+    docker run -e SRA=SRR114482$i -v $pathfastq:/root/ncbi/public/sra 66ca1b570a7a && echo "$SRA"' sra->fastq succesfully' || echo  "$SRA"' fail to donwload'
     
 #>>>>>>>triming of files <Cutadapt>
 
-    docker run -e SRA -e settings -v $pathfastq:/mnt 91e48802e6a3 && echo "$SRA"' succesfully trimmed! ' || echo  "$SRA"' fail to trim '
+    docker run -e SRA=SRR114482$i -e settings -v $pathfastq:/mnt 91e48802e6a3 && echo "$SRA"' succesfully trimmed! ' || echo  "$SRA"' fail to trim '
 done
 
 #>>>>>>>list of files to be align with their barcode
@@ -30,7 +29,7 @@ ls $pathfastq  -1 > $pathfastq/config0.txt && grep "trimmed" $pathfastq/config0.
 
 #>>>>>>>alignment using <miRDeep2>
 
-docker run -v $pathIndex:/mnt/index -v $pathfastq:/mnt 891318f9b55d #-it 9a61a41ee404 #original hayque intalar unas librerias
+docker run -v $pathIndex:/mnt/index -v $pathfastq:/mnt 92deee3a7a3f #-it 9a61a41ee404 #original hayque intalar unas librerias
 
 #>>>>>>>diferential expresion
 
